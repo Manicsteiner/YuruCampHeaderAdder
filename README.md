f3c0e2930db794fd333952e028c707ea30dcc6fb
Gunoshozo <51006468+Gunoshozo@users.noreply.github.com>
2024-05-27 17:18:44 +0400

    Support for bc7 textures and get image dimensions from original file (#2)
    * -support bc7 format for textures with alpha
    -use template files and read dimensions from original files
    
    * -update readme
    
    ---------
    
    Co-authored-by: Gunoshozo <gunoshozo@gmail.com>

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # YuruCampHeaderAdder
 Image tool for game "Yuru Camp Have a nice day"&amp;"Summertime Render Another Horizon" (Switch)
 Those pictures are DDS files without header, just add a header to it is okay.
-Rename the target header file into "header" before runing the script.
-For _YuruCamp_, header-1080 for bg, eg. header-2700 for eg/EG_069_A0, EG_070_A0, EG_071_A0, EG_072_A0, EG_073_A0, EG_074_A0.
-For _Summer Time Rendering_, header-1080 for bg, eg.
-Still not know how to deal with bu and sp files, you can get some correct shapes with header-4096/header-4096r, but the color is incorrect.
+Script will identify dimensions of a texture from original file's footer.
+
+For BG and EG use following command:
+```
+python headeradder.py "path/to/file1" "path/to/file2" ...
+```
+
+For SP, BU and MG (STRAH only) use following command:
+```
+python headeradder.py -alpha "path/to/file1" "path/to/file2" ...
+```
--- headeradder.py
+++ headeradder.py
@@ -3,26 +3,51 @@ from io import BytesIO
 
 from PIL import Image
 
+
+def build_header(width, height, has_alpha):
+    if has_alpha:
+        header_file = "header-bc7-template"
+    else:
+        header_file = "header-dxt1-template"
+    with open(header_file, mode="rb") as f:
+        header_bytes = bytearray(f.read())
+    width_bytes = width.to_bytes(4, byteorder="little", signed=False)
+    height_bytes = height.to_bytes(4, byteorder="little", signed=False)
+    header_bytes[0xc:0x10] = height_bytes
+    header_bytes[0x10:0x14] = width_bytes
+    return header_bytes
+
+
 def getFileNameWithoutExtension(path):
     return path.split('\\').pop().split('/').pop().rsplit('.', 1)[0]
 
-def headeradder(file):
+
+def getDimensionsFromFooter(file_bytes: bytes):
+    arr = bytearray(file_bytes)[-0x50:]
+    width = int.from_bytes(arr[:4], byteorder="little", signed=False)
+    height = int.from_bytes(arr[4:8], byteorder="little", signed=False)
+    return (width, height)
+
+
+def headeradder(file, has_alpha=False):
     fl = open(file, 'rb')
-    header = open("header", 'rb')
+    file_bytes = fl.read()
+    (width, height) = getDimensionsFromFooter(file_bytes)
+    header = build_header(width, height, has_alpha)
     filename = getFileNameWithoutExtension(file)
     #wrf = open(file+".dds", 'wb')
     wrf = BytesIO()
-    wrf.write(header.read())
-    wrf.write(fl.read())
+    wrf.write(header)
+    wrf.write(file_bytes)
     fl.close()
-    header.close()
     pic = Image.open(wrf)
     pic = pic.transpose(Image.Transpose.FLIP_TOP_BOTTOM)
-    #pic = pic.transpose(Image.Transpose.ROTATE_180)
+    # pic = pic.transpose(Image.Transpose.ROTATE_180)
     pic.save(filename + '.png', 'png')
     wrf.close()
     print("ddsheaderadder: '" + file + "' convert png success! Save as '" + filename + '.png' + "'")
-    
+
+
 def ddscov(file):
     pic = Image.open(file)
     pic.transpose(Image.FLIP_TOP_BOTTOM)
@@ -30,9 +55,14 @@ def ddscov(file):
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2 :
+    if len(sys.argv) < 2:
         exit()
-    files=[]
-    files=sys.argv[1:]
+
+    has_alpha = sys.argv[1] == "-alpha"
+    if has_alpha:
+        files = sys.argv[2:]
+    else:
+        files = sys.argv[1:]
+
     for file in files:
-        headeradder(file)
+        headeradder(file, has_alpha)
